243817a1b2d8b8526fddb54789fed36ec7f0f67e
Dennis <dennis1dittmann@gmail.com>
2021-05-22 22:10:07 +0200

    added on_post

--- riskanalysis/riskanalysis/resources.py
+++ riskanalysis/riskanalysis/resources.py
@@ -16,8 +16,22 @@ class Resource:
         doc = {
             'images': [
                 {
-                    'href': '/home/dennis/Pictures/Depot.png'
+                    'href': 'https://dittmann.io/'
                 }
             ]
         }
         resp.body = json.dumps(doc, ensure_ascii=False)
+
+    async def on_post(self, req, resp, user_id):
+        try:
+            doc = req.context.Document
+        except AttributeError:
+            raise falcon.HTTPBadRequest(
+                title='Missing thing',
+                description='A thing must be submitted int the request body.'
+            )
+
+        proper_thing = await self.db.add_thing(doc)
+
+        resp.status = falcon.HTTP_201
+        resp.location = '/%s/things/%s' % (user_id, proper_thing['id'])
