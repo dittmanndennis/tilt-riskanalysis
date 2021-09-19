7396c1997362cbf4390dc0c7fa168b8654746fbe
Dennis <dennis1dittmann@gmail.com>
2021-09-19 20:28:22 +0200

    cleaned

--- riskanalysis/src/controller/controller.py
+++ riskanalysis/src/controller/controller.py
@@ -65,15 +65,4 @@ class Controller(object):
                 Graph().writeHarmonicClosenessCluster(c)
 
     def getRiskScore(self, domain):
-        find = FindTILTs()
-        sharing = SharingNetworks()
-
-        properties = find.findProperties(domain)
-        childNodes = sharing.getChildNodes(domain)
-
-        if properties is not None and (len(properties)-1 is not len(childNodes) or len(childNodes) == 0):
-            connections = find.findConnections(domain)
-            #print(properties)
-            sharing.createSharingNetwork(properties, connections)
-
         return Graph().similarityProbability(domain)
